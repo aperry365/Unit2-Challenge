0f51e991921400f0b0a442dc80856d7bdbc8d63a
aperry365 <b00142477@mytudublin.ie>
2023-10-20 21:20:32 +0100

    Challenge 2 Completed
    Upload of completed challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogLimit = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogLimit <= 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogLimit = 1;
+        }
+        if (dogLimit >= 0) {
+        dogLimit -= Time.deltaTime;
+        }
+    }
+}
